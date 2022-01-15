1016cf282282d955ee86cc2abab411f8e89ae4db
DanicaRad <87402525+DanicaRad@users.noreply.github.com>
2022-01-14 17:27:27 -0800

    completed assignemnt

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,21 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import *
+
+app = Flask(__name__)
+
+@app.route('/<math>')
+def do_math_query_only(math):
+    math = eval(math)
+    a = request.args['a']
+    b = request.args['b']
+
+    return str(math(a, b))
+
+@app.route('/math/<math>')
+def do_math(math):
+    math = eval(math)
+    a = request.args['a']
+    b = request.args['b']
+
+    return str(math(a, b))
